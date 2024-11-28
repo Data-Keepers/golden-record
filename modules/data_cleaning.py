b20e38c898efef706284db96ccfacf5f422a8863
Nikita Stasevich <n.stasevich@facct.ru>
2024-11-28 16:10:47 +0300

    fix: improve validating fio

--- modules/data_cleaning.py
+++ modules/data_cleaning.py
@@ -7,11 +7,14 @@ import pandas as pd
 def clean_data(df: pd.DataFrame) -> pd.DataFrame:
     logging.info("Starting to clean data...")
     start_time = time.time()
-    # удаляем строки с цифрами
-    df = df[~df['client_first_name'].astype(str).str.contains(r'[0-9]', regex=True)]
-    df = df[~df['client_last_name'].astype(str).str.contains(r'[0-9]', regex=True)]
-    df = df[~df['client_middle_name'].astype(str).str.contains(r'[0-9]', regex=True)]
-    df = df[~df['client_fio_full'].astype(str).str.contains(r'[0-9]', regex=True)]
+
+    # Регулярное выражение для разрешённых символов (русские, английские буквы и пробелы)
+    allowed_pattern = r'^[a-zA-Zа-яА-ЯёЁ\s]+$'
+
+    df = df[df['client_first_name'].astype(str).str.match(allowed_pattern, na=False)]
+    df = df[df['client_last_name'].astype(str).str.match(allowed_pattern, na=False)]
+    df = df[df['client_middle_name'].astype(str).str.match(allowed_pattern, na=False)]
+    df = df[df['client_fio_full'].astype(str).str.match(allowed_pattern, na=False)]
 
     logging.info(f"Data cleaning completed, elapsed time: {round(time.time() - start_time, 3)} seconds")
     return df
