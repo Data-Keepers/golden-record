1db04f39f16a7bcf1c7e09a677b70e6062c3b58c
m.dzyuba <m.dzyuba@1440.space>
2024-11-28 10:39:37 +0300

    добавлены действия для очистки

--- modules/data_cleaning/data_cleaning.py
+++ modules/data_cleaning/data_cleaning.py
@@ -5,5 +5,8 @@ import pandas as pd
 
 def clean_data(df: pd.DataFrame) -> pd.DataFrame:
     logging.info("Starting to clean data...")
-
+    # удаляем строки с цифрами
+    df = df[~df['client_first_name'].astype(str).str.contains(r'[0-9]', regex=True)]
+    df = df[~df['client_last_name'].astype(str).str.contains(r'[0-9]', regex=True)]
+    df = df[~df['client_middle_name'].astype(str).str.contains(r'[0-9]', regex=True)]
     return df
